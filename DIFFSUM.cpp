fcebce37a92ebc0cab21366c46ea7847f2ba684b
PhantomCracker <popa.vlad94@gmail.com>
2020-03-05 13:06:54 +0200

    //

---
+++ DIFFSUM.cpp
@@ -0,0 +1,17 @@
+//
+// Created by phantomcracker on 05.03.2020.
+//
+
+#include <iostream>
+
+int main() {
+    int n1, n2;
+    std::cin >> n1 >> n2;
+    int result;
+    n1 > n2 ? result = n1 - n2 : result = n1 + n2;
+
+    std::cout << result << std::endl;
+
+    std::cout << std::endl;
+    return 0;
+}
