9c2eea8e72bbe68164a8f7f2500e6a7366f32a8a
PhantomCracker <popa.vlad94@gmail.com>
2020-03-05 13:07:49 +0200

    Sum or difference - Done

--- DIFFSUM.cpp
+++ DIFFSUM.cpp
@@ -7,9 +7,9 @@
 int main() {
     int n1, n2;
     std::cin >> n1 >> n2;
+
     int result;
     n1 > n2 ? result = n1 - n2 : result = n1 + n2;
-
     std::cout << result << std::endl;
 
     std::cout << std::endl;
