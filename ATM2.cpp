f1e0e70f5e08c98ef04bf50ad089c7e98e99a53e
PhantomCracker <popa.vlad94@gmail.com>
2021-01-25 20:42:02 +0200

    Adding .gitignore file

--- ATM2.cpp
+++ ATM2.cpp
@@ -2,3 +2,25 @@
 // Created by PhantomCracker on 1/25/2021.
 //
 
+#include <iostream>
+
+int main() {
+    int T;
+    std::cin >> T;
+    while (T--) {
+        int N, K;
+        std::cin >> N >> K;
+        int A[N];
+        for (int i = 0; i < N; i++) {
+            std::cin >> A[i];
+            if (K - A[i] >= 0) {
+                K = K - A[i];
+                std::cout << "1";
+            } else {
+                std::cout << "0";
+            }
+        }
+    }
+    std::cout << std::endl;
+    return 0;
+}
