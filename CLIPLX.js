0192fe6f2ecf457b44667d72c7330fa4db2201d6
Popa Vlad <popa.vlad94@gmail.com>
2021-11-17 11:28:46 +0200

    IPL and RCB

---
+++ CLIPLX.js
@@ -0,0 +1,29 @@
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+
+let userInput = '';
+let userInputArray = '';
+let currentLine = 0;
+process.stdin.on('data', (data) => {
+    userInput += data;
+});
+
+process.stdin.on('end', () => {
+    userInputArray = userInput.split('\n');
+    let T = parseInt(readLine());
+    while (T--) {
+        let currentLine = readLine().split(' ');
+        let X = parseInt(currentLine[0]);
+        let Y = parseInt(currentLine[1]);
+
+        console.log(IPLandRCB(X, Y));
+    }
+})
+
+function readLine() {
+    return userInputArray[currentLine++];
+}
+
+function IPLandRCB(X, Y) {
+    return X <= Y ? 0 : (X - Y);
+}
